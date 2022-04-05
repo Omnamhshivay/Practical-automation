ef2d58e30f7ec398b9e46883fc8ab8e6db6ef98c
Omnamshshivay <manshi.patel1987@yahoo.com>
2022-04-05 20:40:54 +0100

    automation

---
+++ src/automation/Variable_Q2.java
@@ -0,0 +1,15 @@
+package automation;
+
+public class Variable_Q2 {
+    int id=101;
+    String name= "Mitesh patel";
+    public void display(){
+        System.out.println(id);
+        System.out.println(name);
+    }
+
+    public static void main(String[] args) {
+        Variable_Q2 obj1 =new Variable_Q2();
+        obj1.display();
+    }
+}
